a5f35db1ae16030fa8c1f309f5b2f8ae4b78d526
Wima S.Y <wima.sy@gmail.com>
2022-06-17 09:08:56 +0700

    Delete visualization directory

--- visualization/mplfinance1.py
+++
@@ -1,5 +0,0 @@
-import yfinance as yf
-import mplfinance as mp
-
-data = yf.download("BBCA",start="2021-08-01", end="2021-09-1")
-mp.plot(data)
--- visualization/mplfinance2.py
+++
@@ -1,6 +0,0 @@
-import yfinance as yf
-import mplfinance as mp
-
-data = yf.download("BBCA.JK",start="2020-08-01", end="2021-09-1")
-
-mp.plot(data,type='candle',mav=(5,10,20),volume=True)
